733faf6fcca5c84a9265b891c1b7a942bedd0e1a
Juano <Juano@DESKTOP-48VCEGK>
2022-10-16 16:39:35 -0500

    Terminado

--- src/main/java/com/mycompany/deber2/Deber2.java
+++ src/main/java/com/mycompany/deber2/Deber2.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.deber2;
 
 /**
@@ -12,24 +11,50 @@ package com.mycompany.deber2;
 public class Deber2 {
 
     public static void main(String[] args) {
-    
-        var empleado1= new Empleado();
-        empleado1.nombre="Juan";
-        empleado1.horasMes=200;
-        empleado1.costoHora=2.5;
-        empleado1.yearIngreso=2010;
-        
+
+        var empleado1 = new Empleado();
+        empleado1.nombre = "Juan Mendoza";
+        empleado1.horasMes = 160;
+        empleado1.costoHora = 1.8;
+        empleado1.yearIngreso = 2010;
+
         System.out.println(empleado1.mostrarInfo());
-        System.out.println("El empleado tiene: "+empleado1.calcularTiempo(2022)+
-        " años en la empresa"+" gana: "+empleado1.calcularIngreso()+" USD al mes"
-                +" con extras gana "+empleado1.calcularSueldo(2022)+
-                " USD al mes,"+" tiene un bono de "+empleado1.calcularBono()+
-                " USD"+" y paga: "+empleado1.calcularImpuesto()+" USD de impuestos.");
-        
-//System.out.println("El empleado gana: "+empleado1.calcularIngreso()+" USD al mes");
-//System.out.println("El empleado gana con extras: "+empleado1.calcularSueldo(2022)+" USD al mes");
-//System.out.println("El empleado tiene un bono: "+empleado1.calcularBono()+" USD");
-//System.out.println("El empleado paga: "+empleado1.calcularImpuesto()+" USD de impuestos");
-        System.out.println("El empleado en total gana: "+empleado1.calcularTotal(2022)+" USD ");
+        System.out.println("El empleado tiene: " + empleado1.calcularTiempo(2022)
+                + " años en la empresa" + " gana: " + empleado1.calcularIngreso() + " USD al mes"
+                + " con extras gana " + empleado1.calcularSueldo(2022)
+                + " USD al mes," + " tiene un bono de " + empleado1.calcularBono()
+                + " USD" + " y paga: " + empleado1.calcularImpuesto(250, 500, 750) + " USD de impuestos.");
+        System.out.println("El empleado en total gana: " + empleado1.calcularTotal(2022, 250, 500, 750) + " USD ");
+        System.out.println("");
+
+        var empleado2 = new Empleado();
+        empleado2.nombre = "Liz Alvarez";
+        empleado2.horasMes = 200;
+        empleado2.costoHora = 2.7;
+        empleado2.yearIngreso = 2017;
+
+        System.out.println(empleado2.mostrarInfo());
+        System.out.println("El empleado tiene: " + empleado2.calcularTiempo(2022)
+                + " años en la empresa" + " gana: " + empleado2.calcularIngreso() + " USD al mes"
+                + " con extras gana " + empleado2.calcularSueldo(2022)
+                + " USD al mes," + " tiene un bono de " + empleado2.calcularBono()
+                + " USD" + " y paga: " + empleado2.calcularImpuesto(250, 500, 750) + " USD de impuestos.");
+        System.out.println("El empleado en total gana: " + empleado2.calcularTotal(2022, 250, 500, 750) + " USD ");
+        System.out.println("");
+
+        var empleado3 = new Empleado();
+        empleado3.nombre = "Pedro Gonzales";
+        empleado3.horasMes = 220;
+        empleado3.costoHora = 3.5;
+        empleado3.yearIngreso = 2013;
+
+        System.out.println(empleado3.mostrarInfo());
+        System.out.println("El empleado tiene: " + empleado3.calcularTiempo(2022)
+                + " años en la empresa" + " gana: " + empleado3.calcularIngreso() + " USD al mes"
+                + " con extras gana " + empleado3.calcularSueldo(2022)
+                + " USD al mes," + " tiene un bono de " + empleado3.calcularBono()
+                + " USD" + " y paga: " + empleado3.calcularImpuesto(250, 500, 750) + " USD de impuestos.");
+        System.out.println("El empleado en total gana: " + empleado3.calcularTotal(2022, 250, 500, 750) + " USD ");
+        System.out.println("");
     }
 }
--- src/main/java/com/mycompany/deber2/Empleado.java
+++ src/main/java/com/mycompany/deber2/Empleado.java
@@ -9,67 +9,77 @@ package com.mycompany.deber2;
  * @author Juano
  */
 public class Empleado {
+
     String nombre;
     int horasMes;
     double costoHora;
     int yearIngreso;
-   
-public String mostrarInfo() {
-        var retorno = "El empleado "+this.nombre + " gana " 
-        + this.costoHora+" USD por hora" + " y trabaja "+this.horasMes+" horas al mes";
-        return retorno;
-}
-    
-public int calcularTiempo(int yearActual){
-        var retorno=0;    
-        retorno =yearActual- this.yearIngreso;
-        return retorno;
- 
-}
- public double calcularIngreso(){
-        var retorno=0.0d;
-        retorno = this.horasMes*this.costoHora;
+
+    public String mostrarInfo() {
+        var retorno = "El empleado " + this.nombre + " gana "
+                + this.costoHora + " USD por hora" + " y trabaja " + this.horasMes + " horas al mes";
         return retorno;
     }
 
- public double calcularSueldo(int yearActual){
-        var retorno=0.0d;
-        var edad=this.calcularTiempo(yearActual)*0.02;
-        retorno = this.calcularIngreso()*edad+this.calcularIngreso();
+    public int calcularTiempo(int yearActual) {
+        var retorno = 0;
+        retorno = yearActual - this.yearIngreso;
         return retorno;
-    }
- //<>
- public double calcularBono(){
-        var retorno=0.0d;
-        if(this.horasMes>160){retorno= ((this.horasMes-160)*(this.costoHora*2));}
-        else { retorno = 0;
-       }
- 
-        return retorno;
-    }
- 
- public double calcularImpuesto(){
-        var retorno=0.0d;
-        var sueldo=this.horasMes*this.costoHora;
-        
-        if (sueldo<100){retorno=0;}
-        else{if(sueldo>=100 && sueldo<200){
-            retorno=sueldo*0.05;}
-        else{if(sueldo>=200 && sueldo<300){
-            retorno=sueldo*0.12;}
-        else{if(sueldo>=300){
-            retorno=sueldo*0.25;}
-    
-        }}}
-        return retorno;
-    }
- 
- public double calcularTotal(int yearActual){
-        var retorno=0.0d;
-        retorno =this.calcularSueldo(yearActual)+this.calcularBono()-this.calcularImpuesto();
- 
-        return retorno;
-    }
- }
- 
 
+    }
+
+    public double calcularIngreso() {
+        var retorno = 0.0d;
+        retorno = this.horasMes * this.costoHora;
+        return retorno;
+    }
+
+    public double calcularSueldo(int yearActual) {
+        var retorno = 0.0d;
+        var edad = this.calcularTiempo(yearActual) * 0.02;
+        retorno = this.calcularIngreso() * edad + this.calcularIngreso();
+        return retorno;
+    }
+    //<>
+
+    public double calcularBono() {
+        var retorno = 0.0d;
+        if (this.horasMes > 160) {
+            retorno = ((this.horasMes - 160) * (this.costoHora * 2));
+        } else {
+            retorno = 0;
+        }
+
+        return retorno;
+    }
+
+    public double calcularImpuesto(int limite1, int limite2, int limite3) {
+        var retorno = 0.0d;
+        var sueldo = this.horasMes * this.costoHora;
+
+        if (sueldo < 250) {
+            retorno = 0;
+        } else {
+            if (sueldo >= 250 && sueldo < 500) {
+                retorno = sueldo * 0.05;
+            } else {
+                if (sueldo >= 500 && sueldo < 750) {
+                    retorno = sueldo * 0.12;
+                } else {
+                    if (sueldo >= 750) {
+                        retorno = sueldo * 0.25;
+                    }
+
+                }
+            }
+        }
+        return retorno;
+    }
+
+    public double calcularTotal(int yearActual, int limite1, int limite2, int limite3) {
+        var retorno = 0.0d;
+        retorno = this.calcularSueldo(yearActual) + this.calcularBono() - this.calcularImpuesto(limite1, limite2, limite3);
+
+        return retorno;
+    }
+}
